47e4a6fe271d5e8747bb6f36f6813817c0f82066
Leandro <leandro_ccb10@hotmail.com>
2021-06-30 09:54:21 -0300

    adicionado rota resource para CRUD de grupo

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CadGrupoProdutoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('grupo', CadGrupoProdutoController::class);
