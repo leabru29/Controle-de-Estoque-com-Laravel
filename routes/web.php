2fa5e3ad3450a243a7796ce30729233b7c70e1a3
Leandro <leandro_ccb10@hotmail.com>
2021-07-09 12:21:40 -0300

    Adicionando novas rotas resources

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CadGrupoProdutoController;
-use App\Http\Controllers\MarcaController;
 use App\Http\Controllers\ArmazenamentoController;
+use App\Http\Controllers\CadGrupoProdutoController;
+use App\Http\Controllers\CidadesController;
+use App\Http\Controllers\EstadosController;
+use App\Http\Controllers\FornecedoresController;
+use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\MedidaController;
+use App\Http\Controllers\PaisesController;
+use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\SaidaController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,8 +28,22 @@ Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::resource('armazenamento', ArmazenamentoController::class);
+
 Route::resource('grupo', CadGrupoProdutoController::class);
+
+Route::resource('cidade', CidadesController::class);
+
+Route::resource('estado', EstadosController::class);
+
+Route::resource('fornecedor', FornecedoresController::class);
 
 Route::resource('marca', MarcaController::class);
 
-Route::resource('armazenamento', ArmazenamentoController::class);
+Route::resource('unidade', MedidaController::class);
+
+Route::resource('paises',PaisesController::class);
+
+Route::resource('produtos',ProdutoController::class);
+
+Route::resource('saida',SaidaController::class);
